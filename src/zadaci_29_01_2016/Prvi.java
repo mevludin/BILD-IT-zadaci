0e97f13c5c059040db226c08d4eef7123c3aa718
mevludin <mevludin.s@gmail.com>
2016-02-03 19:29:16 +0100

    Zadaca 29_01_2016

--- src/zadaci_29_01_2016/Prvi.java
+++ src/zadaci_29_01_2016/Prvi.java
@@ -2,4 +2,15 @@ package zadaci_29_01_2016;
 
 public class Prvi {
 
+	public static void main(String[] args) {
+		for (int i=1;i<=12;i++){
+			//mjeseci smjesteni u niz stringova
+		String []mjeseci={"","Januar","Februar","Mart","April","Maj","Juni","Juli",
+				"Avgust","Septembar","Oktobar","Novembar","Decembar"};
+		int brMj=(int)((Math.random()*12)+1);//nasumican br
+		//poziv mjeseca iz niza
+System.out.println("Mjesec "+mjeseci[brMj]+" je "+brMj+". mjesec u godini!");
+		}
+	}
+
 }
